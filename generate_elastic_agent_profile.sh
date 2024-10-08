af399145cf03441d6f7c347ae9833dfdd9b5bc89
HyunSu1768 <azxcv1768@gmail.com>
2024-10-08 19:38:10 +0900

    feat: add script to generate Elastic Agent profiles for different environments
    This script automates the creation of pipeline configuration files for
    Elastic Agent based on the specified environments and services.

---
+++ generate_elastic_agent_profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASE_PATH="pipelines"
+CHART_PATH="templates/gocd"
+ENVIRONMENTS=("prod" "stag")
+
+for ENV in "${ENVIRONMENTS[@]}"; do
+  for SERVICE in $(ls "$BASE_PATH/$ENV"); do
+    pwd
+    VALUES_FILE="./$BASE_PATH/$ENV/$SERVICE/values.yaml"
+    OUTPUT_FILE="./$BASE_PATH/$ENV/$SERVICE/$SERVICE-$ENV-pipeline.gocd.yaml"
+
+    ./split_yaml_by_source.sh "$(helm template $SERVICE $CHART_PATH -f $VALUES_FILE)" gocd/templates/pipeline.yaml > $OUTPUT_FILE
+  done
+done
