4dd14aff589430a8cbd4fc16aba015a25e4b7a0f
HyunSu1768 <azxcv1768@gmail.com>
2024-10-22 09:18:50 +0900

    fix(docker_build.sh): move directory change command after build_dir processing to ensure correct path handling

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -10,11 +10,11 @@ echo "$TEMPLATE_JSON"
 
 BUILD_DIR=$(echo "$TEMPLATE_JSON" | jq -r '.build_dir // "/"')
 
-cd "$BUILD_DIR" || { echo "디렉토리 이동 실패: $BUILD_DIR"; exit 1; }
-
 if [[ "$BUILD_DIR" == /* ]]; then
   BUILD_DIR=$(echo "$BUILD_DIR" | sed "s|$(pwd)/||")
 fi
+
+cd "$BUILD_DIR" || { echo "디렉토리 이동 실패: $BUILD_DIR"; exit 1; }
 
 REPO_NAME=${SERVICE_NAME}-${ENVIRONMENT}
 RANDOM_TAG=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
