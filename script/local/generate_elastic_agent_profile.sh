c26807be99f9369146d400d7e9d82d5eea4a8b69
HyunSu1768 <azxcv1768@gmail.com>
2025-01-03 17:33:22 +0900

    refactor(generate_elastic_agent_profile.sh): 에이전트 프로필 생성을 위한 스크립트 리팩토링 에이전트 프로필 생성을 별도의 스크립트(create_agent_profile.sh)로 분리하여 코드 가독성을 향상시키고 유지보수를 용이하게 함.

--- script/local/generate_elastic_agent_profile.sh
+++ script/local/generate_elastic_agent_profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BASE_PATH="pipelines"
-CHART_PATH="templates/gocd"
+BASE_PATH="../pipelines"
+CHART_PATH="../templates/gocd"
 ENVIRONMENTS=("prod" "stag")
 
 function check_and_install_yq {
@@ -25,68 +25,6 @@ check_and_install_yq
 
 for ENV in "${ENVIRONMENTS[@]}"; do
     for SERVICE in $(ls "$BASE_PATH/$ENV"); do
-        pwd
-        VALUES_FILE="./$BASE_PATH/$ENV/$SERVICE/values.yaml"
-        NAME=$(yq eval '.name' "$VALUES_FILE")
-        ENVIRONMENT=$(yq eval '.environment' "$VALUES_FILE")
-        BUILD_PROFILE_ID=$NAME-$ENVIRONMENT-agent-profile
-        echo $BUILD_PROFILE_ID
-
-        POD_TEMPLATE=$(./split_yaml_by_source.sh "$(helm template "$SERVICE" "$CHART_PATH" -f "$VALUES_FILE")" gocd/templates/elastic-agent.yaml | awk '{printf "%s\\n", $0}' | sed 's/"/\\"/g')
-
-        ETAG=$(curl -s -I -X GET "https://gocd.xquare.app/go/api/elastic/profiles/$BUILD_PROFILE_ID" \
-               -u "${GOCD_USERNAME}:${GOCD_PASSWORD}" \
-               -H 'Accept: application/vnd.go.cd.v2+json' | grep etag | awk '{print $2}' | tr -d '\r')
-
-        echo "Fetched ETag: $ETAG"
-
-        curl "https://gocd.xquare.app/go/api/elastic/profiles/$BUILD_PROFILE_ID" \
-             -u "${GOCD_USERNAME}:${GOCD_PASSWORD}" \
-             -H 'Accept: application/vnd.go.cd.v2+json' \
-             -H 'Content-Type: application/json' \
-             -H "If-Match: $ETAG" \
-             -X PUT -d "{
-               \"id\": \"$BUILD_PROFILE_ID\",
-               \"cluster_profile_id\": \"k8-cluster-profile\",
-               \"plugin_id\": \"cd.go.contrib.elasticagent.kubernetes\",
-               \"properties\": [
-                 {
-                   \"key\": \"Image\"
-                 },
-                 {
-                   \"key\": \"MaxMemory\"
-                 },
-                 {
-                   \"key\": \"MaxCPU\"
-                 },
-                 {
-                   \"key\": \"Environment\"
-                 },
-                 {
-                   \"key\": \"PodConfiguration\",
-                   \"value\": \"$POD_TEMPLATE\"
-                 },
-                 {
-                   \"key\": \"SpecifiedUsingPodConfiguration\"
-                 },
-                 {
-                   \"key\": \"PodSpecType\",
-                   \"value\": \"yaml\"
-                 },
-                 {
-                   \"key\": \"RemoteFile\"
-                 },
-                 {
-                   \"key\": \"RemoteFileType\",
-                   \"value\": \"json\"
-                 },
-                 {
-                   \"key\": \"Privileged\"
-                 }
-               ]
-             }"
-
-        ./split_yaml_by_source.sh "$(helm template "$SERVICE" "$CHART_PATH" -f "$VALUES_FILE")" server/templates/elastic-agent-pvc.yaml > elastic-agent-pvc.yaml
-        kubectl apply -f elastic-agent-pvc.yaml
+        ./create_agent_profile.sh $SERVICE
     done
 done
